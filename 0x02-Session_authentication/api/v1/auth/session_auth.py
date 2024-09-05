da7fd63ef66074b81d889a14a62cae38d8917b9c
mariusbayizere <bayizeremarius119@gmail.com>
2024-09-05 11:05:02 +0200

    push-to-git

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,11 +3,29 @@
 Session Authentication module
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session authentication class that inherits from Auth.
-    Currently.
     """
-    pass
+
+    # Class attribute to store session data
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a given user_id.
+        Returns:
+            Session ID if successful, None otherwise.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        # Generate a unique session ID using uuid4
+        session_id = str(uuid.uuid4())
+
+        # Store the session_id with the associated user_id in the dictionary
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
