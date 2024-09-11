ecc15b3d650ff895950a2324b788e1278e1d4177
bayizeremarius <bayizeremarius119@gmail.com>
2024-09-11 08:00:01 +0200

    Updated !

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 from auth import Auth
 
 app = Flask(__name__)
-AUTH = Auth()  # Instantiate the Auth object
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -14,17 +14,24 @@ def welcome():
 
 @app.route("/users", methods=["POST"])
 def register_user():
-    """Register a user via POST request with 'email' and 'password'"""
+    """
+    Register a new user via POST request.
+    Accepts 'email' and 'password' as form data.
+    """
+    # Get the form data for email and password
     email = request.form.get("email")
     password = request.form.get("password")
 
+    # Check if email or password is missing
     if not email or not password:
         return jsonify({"message": "email and password are required"}), 400
 
     try:
+        # Try to register the user using the AUTH object
         AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"}), 200
-    except ValueError:  # If the user already exists, raise an error
+    except ValueError:
+        # If user already exists, catch the ValueE
         return jsonify({"message": "email already registered"}), 400
 
 
